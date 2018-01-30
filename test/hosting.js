f2d5913a609ca51e624a5b9f00786cce285bef5c
Piet Geursen <pietgeursen@gmail.com>
2018-01-30 21:45:02 +1300

    example

--- test/hosting.js
+++ test/hosting.js
@@ -12,14 +12,13 @@ test('find gets all messages by all authors, hosting gets only messages by me', 
   var katieKey = ssbKeys.generate()
   var sbot = createSbot({temp:'piet', keys: pietKey})
   var katie = sbot.createFeed(katieKey)
-  var piet = sbot.createFeed(pietKey)
 
-  piet.add(validGathering,function(err, data) {})
+  sbot.publish(validGathering,function(err, data) {})
   katie.add(validGathering,function(err, data) {})
 
   pull(sbot.gatherings.find(), pull.take(2), pull.collect(function(err, gatherings) {
     t.notEqual(gatherings.findIndex(function(gathering) {
-      return gathering.value.author ===  piet.id
+      return gathering.value.author ===  pietKey.id
     }), -1, 'message authored by piet')
     t.notEqual(gatherings.findIndex(function(gathering) {
       return gathering.value.author ===  katie.id
@@ -28,7 +27,7 @@ test('find gets all messages by all authors, hosting gets only messages by me', 
     
     pull(sbot.gatherings.hosting({live: false}), pull.collect(function (err, gatherings){
       t.equal(gatherings.length, 1, 'there is only one gathering')
-      t.equal(gatherings[0].value.author, piet.id, 'and piet is the author')
+      t.equal(gatherings[0].value.author, pietKey.id, 'and piet is the author')
       t.end()
       sbot.close()
     }))
